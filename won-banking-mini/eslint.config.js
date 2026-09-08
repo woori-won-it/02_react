d03406e7a06d09cb6a59f6cd64bd10dd2c3048af
yezixx <skylow917@daum.net>
2026-09-08 16:57:31 +0900

    comment: 주석 추가

--- won-banking-mini/eslint.config.js
+++ won-banking-mini/eslint.config.js
@@ -1,21 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]), // 빌드 결과물은 검사하지 않는다
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"], // 검사 대상
     extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      js.configs.recommended, // 자바스크립트 기본 규칙
+      reactHooks.configs.flat.recommended, // 훅 규칙
+      reactRefresh.configs.vite, // Vite 전용 규칙
     ],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: globals.browser, // 브라우저에서 도는 코드다
+      parserOptions: { ecmaFeatures: { jsx: true } }, // JSX 를 읽을 줄 알아야 한다
     },
   },
-])
+]);
